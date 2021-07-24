d6b1e298cf3020468b677c5d2a3fd31879b3e1d4
dj4t9n <pandeyjatin47@gmail.com>
2021-07-24 11:17:18 +0530

    org mode related configs

--- emacs.el
+++ emacs.el
@@ -1,7 +1,7 @@
 (setq inhibit-startup-screen t)
 (scroll-bar-mode -1)
 (set-fringe-mode 10)
-(setq visible-bell t)
+(setq visible-bell nil)
 (menu-bar-mode 0)
 (tool-bar-mode 0)
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
@@ -10,8 +10,8 @@
 ;;use larger font
 (setq default-frame-alist '((font . "Source Code Pro-14")))
 
-(set-frame-parameter (selected-frame) 'alpha '(95 50))
-(add-to-list 'default-frame-alist '(alpha 85 50))
+(set-frame-parameter (selected-frame) 'alpha '(95 70))
+(add-to-list 'default-frame-alist '(alpha 95 70))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -25,13 +25,36 @@
  '(custom-enabled-themes '(misterioso))
  '(custom-safe-themes
    '("2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" default))
- '(package-selected-packages '(doom-modeline solarized-theme evil ##)))
+ '(package-selected-packages
+   '(use-package org-bullets magit doom-modeline solarized-theme evil ##)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(fixed-pitch ((t (:family "Fira Code Retina" :height 160))))
+ '(org-block ((t (:inherit fixed-pitch))))
+ '(org-code ((t (:inherit (shadow fixed-pitch)))))
+ '(org-document-info ((t (:foreground "dark orange"))))
+ '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
+ '(org-document-title ((t (:inherit default :weight bold :foreground "#839496" :family "Sans Serif" :height 2.0 :underline nil))))
+ '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
+ '(org-level-1 ((t (:inherit default :weight bold :foreground "#839496" :family "Sans Serif" :height 1.75))))
+ '(org-level-2 ((t (:inherit default :weight bold :foreground "#839496" :family "Sans Serif" :height 1.5))))
+ '(org-level-3 ((t (:inherit default :weight bold :foreground "#839496" :family "Sans Serif" :height 1.25))))
+ '(org-level-4 ((t (:inherit default :weight bold :foreground "#839496" :family "Sans Serif" :height 1.1))))
+ '(org-level-5 ((t (:inherit default :weight bold :foreground "#839496" :family "Sans Serif"))))
+ '(org-level-6 ((t (:inherit default :weight bold :foreground "#839496" :family "Sans Serif"))))
+ '(org-level-7 ((t (:inherit default :weight bold :foreground "#839496" :family "Sans Serif"))))
+ '(org-level-8 ((t (:inherit default :weight bold :foreground "#839496" :family "Sans Serif"))))
+ '(org-link ((t (:foreground "royal blue" :underline t))))
+ '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-property-value ((t (:inherit fixed-pitch))) t)
+ '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
+ '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
+ '(org-verbatim ((t (:inherit (shadow fixed-pitch)))))
+ '(variable-pitch ((t (:family "ETBembo" :height 180 :weight thin)))))
 
 (require 'ido)
 (ido-mode t)
@@ -44,6 +67,9 @@
 ;;(package-initialize)
 ;;(package-refresh-contents)
 
+(package-install 'use-package)
+(require 'use-package)
+
 (require 'doom-modeline)
 (doom-modeline-mode 1)
 
@@ -55,3 +81,64 @@
 (require 'evil)
 (evil-mode 1)
 (set-keyboard-coding-system nil)
+
+
+;; customizing org mode
+
+;; hide the emphasis markup (e.g. /.../ for italics, *...* for bold, etc.)
+(setq org-hide-emphasis-markers t)
+;;set up a font-lock substitution for list markers
+(font-lock-add-keywords 'org-mode
+												'(("^ *\\([-]\\) "
+													 (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+;; org-bullets package replaces all headline markers with different Unicode bullets
+(use-package org-bullets
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+(let* ((variable-tuple
+        (cond ((x-list-fonts "ETBembo")         '(:font "ETBembo"))
+              ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
+              ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
+              ((x-list-fonts "Verdana")         '(:font "Verdana"))
+              ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
+              (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
+       (base-font-color     (face-foreground 'default nil 'default))
+       (headline           `(:inherit default :weight bold :foreground ,base-font-color)))
+
+  (custom-theme-set-faces
+   'user
+   `(org-level-8 ((t (,@headline ,@variable-tuple))))
+   `(org-level-7 ((t (,@headline ,@variable-tuple))))
+   `(org-level-6 ((t (,@headline ,@variable-tuple))))
+   `(org-level-5 ((t (,@headline ,@variable-tuple))))
+   `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
+   `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.25))))
+   `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.5))))
+   `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.75))))
+   `(org-document-title ((t (,@headline ,@variable-tuple :height 2.0 :underline nil))))))
+
+
+(custom-theme-set-faces
+ 'user
+ '(variable-pitch ((t (:family "ETBembo" :height 180 :weight thin))))
+ '(fixed-pitch ((t ( :family "Fira Code Retina" :height 160)))))
+
+(add-hook 'org-mode-hook 'variable-pitch-mode)
+(add-hook 'org-mode-hook 'visual-line-mode)
+
+(custom-theme-set-faces
+ 'user
+ '(org-block ((t (:inherit fixed-pitch))))
+ '(org-code ((t (:inherit (shadow fixed-pitch)))))
+ '(org-document-info ((t (:foreground "dark orange"))))
+ '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
+ '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
+ '(org-link ((t (:foreground "royal blue" :underline t))))
+ '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-property-value ((t (:inherit fixed-pitch))) t)
+ '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
+ '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
+ '(org-verbatim ((t (:inherit (shadow fixed-pitch))))))
+
