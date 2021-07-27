a6b39841ac00cb415479f730248182444641a965
dj4t9n <pandeyjatin47@gmail.com>
2021-07-28 00:11:37 +0530

    perfect config for org mode

--- emacs.el
+++ emacs.el
@@ -27,34 +27,6 @@
  '(org-fontify-done-headline t)
  '(package-selected-packages
    '(use-package org-bullets magit doom-modeline solarized-theme evil ##)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(fixed-pitch ((t (:family "Fira Code Retina" :height 160))))
- '(org-block ((t (:inherit fixed-pitch))))
- '(org-code ((t (:inherit (shadow fixed-pitch)))))
- '(org-document-info ((t (:foreground "dark orange"))))
- '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
- '(org-document-title ((t (:inherit default :weight bold :foreground "#839496" :family "Sans Serif" :height 2.0 :underline nil))))
- '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
- '(org-level-1 ((t (:inherit default :weight bold :foreground "#839496" :family "Sans Serif" :height 1.75))))
- '(org-level-2 ((t (:inherit default :weight bold :foreground "#839496" :family "Sans Serif" :height 1.5))))
- '(org-level-3 ((t (:inherit default :weight bold :foreground "#839496" :family "Sans Serif" :height 1.25))))
- '(org-level-4 ((t (:inherit default :weight bold :foreground "#839496" :family "Sans Serif" :height 1.1))))
- '(org-level-5 ((t (:inherit default :weight bold :foreground "#839496" :family "Sans Serif"))))
- '(org-level-6 ((t (:inherit default :weight bold :foreground "#839496" :family "Sans Serif"))))
- '(org-level-7 ((t (:inherit default :weight bold :foreground "#839496" :family "Sans Serif"))))
- '(org-level-8 ((t (:inherit default :weight bold :foreground "#839496" :family "Sans Serif"))))
- '(org-link ((t (:foreground "royal blue" :underline t))))
- '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
- '(org-property-value ((t (:inherit fixed-pitch))) t)
- '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
- '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
- '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
- '(org-verbatim ((t (:inherit (shadow fixed-pitch)))))
- '(variable-pitch ((t (:family "ETBembo" :height 180 :weight thin)))))
 
 (require 'ido)
 (ido-mode t)
