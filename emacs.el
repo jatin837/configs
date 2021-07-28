2a90fa79e2a580509a5c52da8e8ae72c126f55c4
dj4t9n <pandeyjatin47@gmail.com>
2021-07-28 23:39:00 +0530

    enable evil redo mode

--- emacs.el
+++ emacs.el
@@ -97,3 +97,12 @@
  ;; If there is more than one, they won't work right.
  )
 (put 'downcase-region 'disabled nil)
+
+
+;; (require 'package)
+;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+;; (package-initialize)
+;; (package-refresh-contents)
+
+;; (package-install 'use-package)
